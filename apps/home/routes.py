756c6042278b3d7ab705779736d9ea29e0057538
Jeann-Carlos <56929989+Jeann-Carlos@users.noreply.github.com>
2022-10-12 12:33:12 -0400

    Adding routs for client services, server, and servers

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -11,18 +11,27 @@ def index():
 
     return render_template('home/index.html', segment='index')
 
-# Format [HOST_ID,HOST_IP,NUMBER_OF_CLIENTS,NUMBER_OF_SERVICES]
+# Format [CLIENT_ID,IP_VPN,NUMBER_OF_CLIENTS,NUMBER_OF_SERVICES]
 def servers_template_handler():
-    cur.execute("SELECT HOST_ID,HOST_IP FROM host")
+    cur.execute("select CLIENT_ID,IP_VPN,count(distinct HOST_ID) connected_devices,count(distinct SERVICIOS_ID) services from (select host.HOST_ID,HOST_IP,CLIENT_ID,IP_VPN,SERVICIOS_ID,PORT,PROTOCOL from host natural join clientes c  left join servicios s on host.HOST_ID = s.HOST_ID) as t1 group by CLIENT_ID")
     server_host_data = cur.fetchall()
-    for i,host_data in enumerate(server_host_data):
-        host_data = list(host_data)
-        cur.execute(f"SELECT count(HOST_ID) from host where HOST_ID={host_data[0]}")
-        host_data.append(cur.fetchall()[0][0])
-        cur.execute(f"SELECT COUNT(SERVICIOS_ID) from servicios where HOST_ID={host_data[0]}")
-        host_data.append(cur.fetchall()[0][0])
-        server_host_data[i] = host_data
     return server_host_data
+
+
+# Format [NAME,DATETIME,STATUS,PORT,PROTOCOLOL,ID]
+def client_services_template_handler(segment):
+    cur.execute(f"select  NOMBRE,TIMESTAMP,STATUS,PORT, PROTOCOL,servicios.SERVICIOS_ID from servicios join host h on servicios.HOST_ID "
+                f"= h.HOST_ID join log_servicios ls on servicios.SERVICIOS_ID = ls.SERVICIOS_ID join clientes c on c.CLIENT_ID = h.CLIENT_ID where HOST_IP='192.0.0.1' and IP_VPN='10.0.0.1' group by ls.SERVICIOS_ID")
+    client_services_data= cur.fetchall()
+    return client_services_data
+
+
+# Format [HOST_IP, NUMBER_OF_LOGS,NUMBER_OF_SERVICES]
+def client_template_handler(segment):
+    cur.execute(f"select HOST_IP,count(distinct LOG_ID), count(SERVICIOS_ID) from host natural join clientes c natural join logs l  left join servicios on host.HOST_ID = servicios.HOST_ID where IP_VPN='{segment}' group by HOST_IP")
+    server_host_data = cur.fetchall()
+    return server_host_data
+
 
 @blueprint.route('/<template>')
 @login_required
@@ -57,8 +66,10 @@ def route_template(template):
 def server_template(server):
 
    try:
+
         segment = get_segment(request)
-        return render_template("home/index.html", segment=segment)
+        client_host_data = client_template_handler(segment)
+        return render_template("home/server.html", segment=segment,client_host_data=client_host_data)
 
    except TemplateNotFound:
         return render_template('home/page-404.html'), 404
@@ -66,6 +77,21 @@ def server_template(server):
    except:
         return render_template('home/page-500.html'), 500
 
+
+@blueprint.route('/servers/<server>/<clientip>')
+@login_required
+def client_services_template(server,clientip):
+
+   try:
+        segment = get_segment(request)
+        client_services_data = client_services_template_handler(segment)
+        return render_template("home/client_services.html",server=server, segment=segment, client_services_data= client_services_data)
+
+   except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+   except:
+        return render_template('home/page-500.html'), 500
 
 # Helper - Extract current page name from request
 def get_segment(request):
