11b5c54d62845231b96ff7a88d9b2af90f7a264f
Jeann-Carlos <jeann.hernandez@upr.edu>
2023-01-02 20:13:45 -0400

    Especial de nuevo de año: working on serverprocess.py and run.py unification

--- serverprocess.py
+++ serverprocess.py
@@ -5,25 +5,7 @@ import datetime
 import sys
 
 import mariadb
-
-try:
-    conn = mariadb.connect(
-        user="root",
-        password="invlab",
-        host="127.0.0.1",
-        port=3306,
-        database="invlab"
-
-    )
-    print(f'Connected to Mariadb: {conn.database}')
-
-    # Get Cursor
-    cur = conn.cursor()
-
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
+from database_cursor import conn,cur
 
 # Cursor to handle SQL queries
 
